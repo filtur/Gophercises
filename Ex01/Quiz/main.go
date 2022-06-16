600540351e8dff882c21b7f651e88dd1fc5c02ad
Phil T <cisphil@yahoo.com>
2022-06-16 10:16:25 -0700

    Refactoring to match excercise solution

--- Ex01/Quiz/main.go
+++ Ex01/Quiz/main.go
@@ -10,6 +10,11 @@ import (
 	"strings"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
 func main() {
 	csvFilename := flag.String("csv", "../problems.csv", "csv file in a format of 'question,answer'")
 	flag.Parse()
@@ -21,18 +26,20 @@ func main() {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
+	lines, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	problems := parseLines(lines)
+
 	reader := bufio.NewReader(os.Stdin)
 	c := 0
-	for i, l := range data {
-		fmt.Printf("Problem #%d: %v\n", i+1, l[0])
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %v\n", i+1, p.q)
 		text, _ := reader.ReadString('\n')
 		text = strings.TrimRight(text, "\r\n")
-		if text == l[1] {
+		if text == p.a {
 			c++
 			fmt.Println("Correct!")
 		} else {
@@ -40,5 +47,16 @@ func main() {
 		}
 	}
 
-	fmt.Printf("You got %d correct out of %v", c, len(data))
+	fmt.Printf("You got %d correct out of %v", c, len(lines))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
 }
